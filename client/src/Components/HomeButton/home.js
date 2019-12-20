4291503efe5ddce2c82bd71297863140fcec5826
Diego Weinmann <diego.weinmann@hotmail.com>
2019-12-19 21:38:14 -0300

    adding changes on login

--- client/src/Components/HomeButton/home.js
+++ client/src/Components/HomeButton/home.js
@@ -12,7 +12,7 @@ class HomeButt extends React.Component {
         console.log(window.history.length);
         
         return (
-            URLactual == 'http://localhost:3000/' ?
+            URLactual == 'http://localhost:3000/home' ?
             <div className="p-2 bg-dark shadow-dark">
                     <footer className="row text-center">
                         <Link className="col text-muted" to="/"></Link>
@@ -29,7 +29,7 @@ class HomeButt extends React.Component {
             <div className="p-2 bg-dark shadow-dark">
                     <footer className = "row text-center mt-1">
                         <div onClick={() => { window.history.go(-1);}} className="col text-white"><i className="fas fa-chevron-left fa-2x"></i></div>
-                        <Link className="col text-white" to="/"><i className="fas fa-home fa-2x"></i></Link>
+                        <Link className="col text-white" to="/home"><i className="fas fa-home fa-2x"></i></Link>
                         <div onClick={() => { window.history.go(1);}} className="col text-white"><i className="fas fa-chevron-right fa-2x"></i></div>
                     </footer>
             </div>
